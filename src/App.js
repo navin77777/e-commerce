075cb18706cfc0e8da3e4a15da4b6ac1074f735e
Navin Kumar <navinkumarbh6@gmail.com>
2021-10-15 11:14:11 +0530

    update menu using withRouter navigation HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "../src/pages/homepage.component";
+
+const HatPage = () => {
+  return (
+    <div>
+      <h1>HatsPage</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatPage} />
+      </Switch>
     </div>
   );
 }
